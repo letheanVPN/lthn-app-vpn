b60dd1087618d9384541c6fe0a78d9e65b9fdd32
valiant1x <valiant1x@users.noreply.github.com>
2019-03-20 03:24:54 -0500

    add windows support for UAC openvpn and interactive service if a user is running with admin rights, we will use our embedded openvpn binary if a user does not have admin rights, we will first check to see if the OpenVPN Interactive Service for Lethean named pipe is avaialble. if so, we will launch a service here (no admin privileges required) if interactive service is not available and we are not running as an admin, we will request elevation to admin rights when launching our embedded openvpn binary

--- lib/service_ovpn.py
+++ lib/service_ovpn.py
@@ -2,6 +2,7 @@ from service import Service
 import config
 import os
 import sys
+import ctypes
 import re
 import log
 import time
@@ -11,6 +12,9 @@ from subprocess import PIPE
 import atexit
 import services
 import time
+import win32pipe
+import win32file
+import pywintypes
 ON_POSIX = 'posix' in sys.builtin_module_names
 
 class ServiceOvpn(Service):
@@ -26,31 +30,117 @@ class ServiceOvpn(Service):
             verb="3"
         else:
             verb="1"
-        if config.Config.SUDO_BIN:
-            cmd = [config.Config.SUDO_BIN, config.Config.CAP.openvpnBin, "--config", self.cfgfile, "--writepid", self.pidfile, "--verb", verb, "--writepid", self.pidfile]
-        else:
-            cmd = [config.Config.CAP.openvpnBin, "--config", self.cfgfile, "--writepid", self.pidfile, "--verb", verb, "--writepid", self.pidfile]
-        os.chdir(self.dir)
-        if (os.path.isfile(self.pidfile)):
-            os.remove(self.pidfile)
-        log.A.audit(log.A.START, log.A.SERVICE, cmd=" ".join(cmd), serviceid=self.id)
-        if self.isClient():
-            if (config.Config.CAP.vpncStandalone):
-                command = cmd[0]
-                if config.Config.CAP.noRun:
-                    log.L.warning("Exiting from dispatcher. Run manually:\n%s" % (" ".join(cmd)))
-                    atexit.unregister(services.SERVICES.stop)
-                    sys.exit()
-                else:
-                    if not os.path.isfile(config.Config.CAP.openvpnBin):
-                        log.L.error("Openvpn binary %s not found. Cannot continue!" % (config.Config.CAP.openvpnBin))
+        # delete old log file
+        try:
+            os.remove(self.logfile)
+        except OSError:
+            pass
+
+        isWindowsWithAdminNeeded = False
+        isWindowsWithInteractivePipeReady = False
+        if config.CONFIG.isWindows():
+            # are we an admin? if so, we can use bundled openvpn bin
+            # if user is not an admin, we need either the openvpn-gui interactive service or admin rights
+            if not ctypes.windll.shell32.IsUserAnAdmin():                
+                # use interactive service named pipe, if it exists
+                interactiveServicePipeExists = False
+                ERROR_FILE_NOT_FOUND = 2
+                ERROR_PIPE_BUSY = 231
+                handlePipe = None
+                try:
+                    log.L.debug("Attempting to connect to Interactive Service...")
+                    # prepare interactive service startup msg request
+                    # https://community.openvpn.net/openvpn/wiki/OpenVPNInteractiveService
+                    workingdir = os.path.split(self.cfgfile)[0]
+                    openvpnoptions = "--config " + self.cfgfile + " --writepid " + self.pidfile + " --verb " + verb
+                    stdin = ""
+                    wzero = u"\u0000"
+                    startupMsg = (workingdir + wzero + openvpnoptions + wzero + stdin + wzero).encode("utf-16-le")
+                    pipeName = r'\\.\pipe\openvpn$Lethean\service'
+
+                    handlePipe = win32file.CreateFile(pipeName, win32file.GENERIC_READ | win32file.GENERIC_WRITE, 0, None, win32file.OPEN_EXISTING, 0, None)
+
+                    if (handlePipe != win32.INVALID_HANDLE_VALUE):
+                        log.L.info("OVPN Lethean service pipe opened")
+                    else:
+                        log.L.error("Failed opening LTHNVPN service pipe: invalid handle")
+
+                        err = ctypes.windll.kernel32.GetLastError()
+                        if (err == ERROR_FILE_NOT_FOUND):
+                            log.L.error("Lethean VPN service not found! Make sure OpenVPN Interactive Service (Lethean) service is running. If service does not exist, please install OpenVPN and run the Install_LTHN_Service script.")
+                        elif (err != ERROR_PIPE_BUSY):
+                            log.L.error("Failed opening LTHNVPN service pipe: %d" % err)
+                        elif ((ctypes.windll.kernel32.WaitNamedPipeA(pipeName, 20000)) == 0):
+                            log.L.error("Failed waiting to open LTHNVPN service pipe!")
+
                         sys.exit(1)
-                    log.L.warning("Running %s and exiting from dispatcher." % (" ".join(cmd)))
-                    os.execv(command, cmd)
-        if not os.path.isfile(config.Config.CAP.openvpnBin):
-            log.L.error("Openvpn binary %s not found. Cannot continue!" % (config.Config.CAP.openvpnBin))
-            sys.exit(1)
-        self.process = Popen(cmd, stdout=PIPE, stderr=PIPE, bufsize=1, close_fds=ON_POSIX)
+
+                    res = win32pipe.SetNamedPipeHandleState(handlePipe, win32pipe.PIPE_READMODE_MESSAGE, None, None)
+                    if res == 0:
+                        log.L.debug("SetNamedPipeHandleState return code: %d" % res)
+
+                    win32file.WriteFile(handlePipe, startupMsg)
+
+                    while True:
+                        resp = win32file.ReadFile(handlePipe, 64*1024)
+                        #resp = resp.decode("utf-16")
+                        log.L.debug("Pipe message received %d" % resp[0])
+                        if (resp[0] == 0):
+                            decoded = resp[1].decode("utf-16-le")
+                            log.L.debug("Pipe data received: %s" % decoded)
+                            if ('Process ID' in decoded.split('\n')[2]):
+                                interactiveServicePipeExists = True
+                                cmd = ''
+                                log.L.info('Success! PID of openvpn is ' + decoded.split('\n')[1])
+                            break
+
+                except pywintypes.error as e:
+                    if (e.args[0] == ERROR_FILE_NOT_FOUND):
+                        log.L.error("Lethean VPN service not found! Make sure OpenVPN Interactive Service (Lethean) service is running. If service does not exist, please install OpenVPN and run the Install_LTHN_Service script.")
+                    else:
+                        log.L.error("Error opening OVPN LTHN service pipe! %d %s %s" % (e.args[0], e.args[1], e.args[2]))
+                finally:
+                    if (handlePipe and handlePipe != win32.INVALID_HANDLE_VALUE):
+                        win32file.CloseHandle(handlePipe)
+                # if interactive service does not exist, request admin privileges to run openvpn
+                if not interactiveServicePipeExists:
+                    log.L.info("We are not an admin and we failed to interact with Lethean VPN service. Requesting admin elevation...")
+                    isWindowsWithAdminNeeded = True
+        if not interactiveServicePipeExists:
+            if config.Config.SUDO_BIN:
+                cmd = [config.Config.SUDO_BIN, config.Config.CAP.openvpnBin, "--config", self.cfgfile, "--writepid", self.pidfile, "--verb", verb]
+            else:
+                cmd = [config.Config.CAP.openvpnBin, "--config", self.cfgfile, "--writepid", self.pidfile, "--verb", verb]
+            os.chdir(self.dir)
+            if (os.path.isfile(self.pidfile)):
+                os.remove(self.pidfile)
+            log.A.audit(log.A.START, log.A.SERVICE, cmd=" ".join(cmd), serviceid=self.id)
+            if self.isClient():
+                if (config.Config.CAP.vpncStandalone):
+                    command = cmd[0]
+                    if config.Config.CAP.noRun:
+                        log.L.warning("Exiting from dispatcher. Run manually:\n%s" % (" ".join(cmd)))
+                        atexit.unregister(services.SERVICES.stop)
+                        sys.exit()
+                    else:
+                        if not os.path.isfile(config.Config.CAP.openvpnBin):
+                            log.L.error("Openvpn binary %s not found. Cannot continue!" % (config.Config.CAP.openvpnBin))
+                            sys.exit(1)
+                        log.L.warning("Running %s and exiting from dispatcher." % (" ".join(cmd)))
+                        os.execv(command, cmd)
+            if not os.path.isfile(config.Config.CAP.openvpnBin):
+                log.L.error("Openvpn binary %s not found. Cannot continue!" % (config.Config.CAP.openvpnBin))
+                sys.exit(1)
+            if (isWindowsWithAdminNeeded):
+                log.L.info("Requesting UAC prompt for %s using params %s" % (cmd[0], ' '.join(cmd[1:])))
+                SW_HIDE = 0
+                result = ctypes.windll.shell32.ShellExecuteW(None, "runas", cmd[0], ' '.join(cmd[1:]), None, SW_HIDE)
+                if (result <= 32):
+                    log.L.error("Failed to launch OpenVPN with admin rights! You must either provide admin rights or install openvpn and Lethean service.")
+                    sys.exit(1)
+            else:
+                self.process = Popen(cmd, stdout=PIPE, stderr=PIPE, bufsize=1, close_fds=ON_POSIX)
+
         self.pid = self.waitForPid()
         log.L.info("Run service %s: %s [pid=%s]" % (self.id, " ".join(cmd), self.pid))
         if not config.CONFIG.isWindows():

9ea7991773be6a6926475d503990ffdc6a49865a
valiant1x <valiant1x@users.noreply.github.com>
2019-03-29 00:12:10 -0500

    remove openvpn autolaunch key many users were not unchecking the option in the openvpn installer to launch the gui on startup, causing some confusion

--- install/win/Install_LTHN_Service.bat
+++ install/win/Install_LTHN_Service.bat
@@ -1,4 +1,12 @@
 @echo off
+:: Delete OpenVPN-GUI Startup key (done as local user)
+set StartupKeyName="HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run"
+REG.exe QUERY %StartupKeyName% /v "OPENVPN-GUI" > NUL
+if %ERRORLEVEL% NEQ 1 (
+	REG.exe DELETE "%StartupKeyName%" /v "OPENVPN-GUI" /F
+	echo OpenVPN startup key deleted.
+)
+
 :: Obtain elevated command prompt
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %USERPROFILE% %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && echo Requesting admin prompt. You may close this window. && exit /B )

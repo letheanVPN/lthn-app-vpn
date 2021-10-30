8df962c4f5f0e9b8c14eb30948edb605510ddbc4
Snider <snider@lt.hn>
2021-10-30 19:15:00 +0100

    windows path fix

--- src/tools/updater.ts
+++ src/tools/updater.ts
@@ -17,22 +17,25 @@ export class LetheanUpdater {
 
 	static async download(args: any) {
 
-		let url, platform = os.platform(), homeDir = os.homeDir();
+		let url, filename, platform = os.platform(), homeDir = os.homeDir();
 
 		console.log(`Downloading files for ${platform}`)
 
 		switch (platform) {
 			case 'darwin':
 				url = LetheanUpdater.downloads.cli.macos
+				filename = url.split('/').pop() ;
 				break;
 			case 'linux':
 				url = LetheanUpdater.downloads.cli.linux
+				filename = url.split('/').pop() ;
 				break;
 			case 'windows':
 				url = LetheanUpdater.downloads.cli.windows
+				filename = url.split('\\').pop() ;
 				break;
 		}
-		let filename = url.split('/').pop() ;
+
 		try {
 
 			const destination: Destination = {

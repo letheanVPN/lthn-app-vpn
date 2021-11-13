88eccee25595a02caa416f7b10a4f8480bfb2abe
Snider <snider@lt.hn>
2021-11-13 11:28:20 +0000

    adds directory listing and fixes file writing
    Signed-off-by: Snider <snider@lt.hn>

--- src/tools/filesystem.ts
+++ src/tools/filesystem.ts
@@ -1,6 +1,9 @@
 import {Command} from 'https://deno.land/x/cliffy/command/mod.ts';
 import * as path from 'https://deno.land/std/path/mod.ts';
 import {StringResponse} from './string-response.ts';
+import {
+	ensureDirSync
+} from 'https://deno.land/std@0.114.0/fs/mod.ts';
 
 export class Filesystem {
 
@@ -24,13 +27,30 @@ export class Filesystem {
 		return Deno.readTextFileSync(Filesystem.path(args.path));
 	}
 
+	static list(args: any) {
+		const ret = [];
+		for (const dirEntry of Deno.readDirSync(Filesystem.path(args.path))) {
+			if (!dirEntry.name.startsWith('.')) {
+				ret.push(dirEntry.name);
+			}
+		}
+		return JSON.stringify(ret);
+	}
+
 	static write(path: string, data: string) {
-		return Deno.writeTextFileSync(Filesystem.path(path), data);
+		ensureDirSync(path);
+		Deno.writeTextFileSync(Filesystem.path(path), data);
+		return '1';
 	}
 
 	public static config() {
 
 		return new Command().description('File System')
+			.command('list', 'List entities in path')
+			.option('--path <string>', 'File path to view')
+			.action((args) => {
+				throw new StringResponse(Filesystem.list(args));
+			})
 			.command('path', 'Returns correct')
 			.option('--convert <string>', 'File path to convert')
 			.action((args) => {
@@ -44,7 +64,9 @@ export class Filesystem {
 			.command('write', 'Write a file')
 			.option('--path <string>', 'File path to read')
 			.option('--data <string>', 'File data to save')
-			.action((args) => Filesystem.write(args.path, args.data));
+			.action((args) => {
+				throw new StringResponse(Filesystem.write(args.path, args.data));
+			});
 	}
 
 }

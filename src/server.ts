fef3bcfaa301654ddd7cf45ba8e855c51cf57fef
Snider <snider@lt.hn>
2021-11-16 10:31:34 +0000

    show --help if lthn.exe run without any commands
    Signed-off-by: Snider <snider@lt.hn>

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,13 @@ import {LetheanCli} from './lethean-cli.ts';
 // @todo adds stdin/tcp detection for rest mapping
 await LetheanCli.init();
 try {
-await LetheanCli.run(Deno.args);
+	let args;
+	if (Deno.args.length === 0) {
+		args = ['--help'];
+	} else {
+		args = Deno.args;
+	}
+	await LetheanCli.run(args);
 } catch (error) {
 
 	console.log(error.message);
